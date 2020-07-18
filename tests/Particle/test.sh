71ef64fdf15e0fc907077c3370c1e3c3d73b9a7a
maxiaoba <maxiaoba@umich.edu>
2020-07-18 16:17:48 -0700

    update tests in ParticleEnv

--- tests/Particle/test.sh
+++ tests/Particle/test.sh
@@ -1,11 +1,6 @@
 for seed in 0 1 2
 do
-	# python masac.py --exp_name simple_spread --epoch 1000 --seed $seed --gpu
-	# python masac.py --online_action --exp_name simple_spread --epoch 1000 --seed $seed --gpu
-	# python maddpg.py --exp_name simple_spread --epoch 1000 --seed $seed --gpu
-	# python maddpg.py --online_action --exp_name simple_spread --epoch 1000 --seed $seed --gpu
-	# python prg_gaussian.py --exp_name simple_spread --epoch 1000 --seed $seed --gpu
-	# python prg_gaussian.py --online_action --exp_name simple_spread --epoch 1000 --seed $seed --gpu
-	# python prg.py --exp_name simple_spread --epoch 1000 --seed $seed --gpu
-	# python prg.py --online_action --exp_name simple_spread --epoch 1000 --seed $seed --gpu
+	# python maddpg.py --exp_name simple_adversary --epoch 1000 --seed $seed --gpu
+	# python masac.py --exp_name simple_adversary --epoch 1000 --seed $seed --gpu
+	python prg_gaussian.py --exp_name simple_adversary --epoch 1000 --online_action --centropy --seed $seed --gpu
 done
