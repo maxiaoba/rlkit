e1b5e322b239c56be97879f6e8f2a8a5532a3075
Xiaobai Ma <xiaobaima@DN2lk5d74.stanford.edu>
2020-11-13 10:36:46 -0800

    same

--- rlkit/torch/prg/prg.py
+++ rlkit/torch/prg/prg.py
@@ -118,7 +118,7 @@ class PRGTrainer(TorchTrainer):
                 self.target_entropy = -np.prod(self.env.action_space.shape).item()  # heuristic value from Tuomas
             if self.use_entropy_loss:
                 # self.log_alpha_n = [ptu.zeros(1, requires_grad=True) for i in range(len(self.policy_n))]
-                self.log_alpha_n = [ptu.tensor([np.log(self.init_alpha)], requires_grad=True) for i in range(len(self.policy_n))]
+                self.log_alpha_n = [ptu.tensor([np.log(self.init_alpha)], requires_grad=True, dtype=torch.float32) for i in range(len(self.policy_n))]
                 self.alpha_optimizer_n = [
                     optimizer_class(
                         [self.log_alpha_n[i]],
@@ -127,7 +127,7 @@ class PRGTrainer(TorchTrainer):
 
             if self.use_cactor_entropy_loss:
                 # self.log_calpha_n = [ptu.zeros(1, requires_grad=True) for i in range(len(self.policy_n))]
-                self.log_calpha_n = [ptu.tensor([np.log(self.init_alpha)], requires_grad=True) for i in range(len(self.policy_n))]
+                self.log_calpha_n = [ptu.tensor([np.log(self.init_alpha)], requires_grad=True, dtype=torch.float32) for i in range(len(self.policy_n))]
                 self.calpha_optimizer_n = [
                     optimizer_class(
                         [self.log_calpha_n[i]],
